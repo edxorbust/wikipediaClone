4fd8d8b75ca1d3bbc2863debacda4405b77ce95e
Eduardo Orbe <eduardoorbeb@gmail.com>
2024-08-07 10:23:27 -0700

    fix url 2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry, name="entry"),
-    path("search/", views.search, name="search"),
-    path("newpage/", views.newpage, name="newpage"),
-    path("edit/", views.edit, name="edit"),
-    path("save_edit/", views.save_edit, name="save_edit"),
-    path("aletorio/", views.aleatorio, name="aleatorio")
+    path('', views.index, name="index"),
+    path('wiki/<str:entry>', views.entry, name="entry"),
+    path('search/', views.search, name="search"),
+    path('newpage/', views.newpage, name="newpage"),
+    path('edit/', views.edit, name="edit"),
+    path('save_edit/', views.save_edit, name="save_edit"),
+    path('aletorio/', views.aleatorio, name="aleatorio")
     ]
